ac302b91a3d4a0c1baef23ea7bfa19b45cf5c285
Tsuser1 <tsusergaming@gmail.com>
2015-11-23 16:13:13 -0800

    THIS IS A BUGGY UPDATE -- DO NOT USE.

--- TsCardinal/src/com/tsuser/tscardinal/Main.java
+++ TsCardinal/src/com/tsuser/tscardinal/Main.java
@@ -1,7 +1,22 @@
 package com.tsuser.tscardinal;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import lib.PatPeter.SQLibrary.Database;
+import lib.PatPeter.SQLibrary.SQLite;
 import net.md_5.bungee.api.ChatColor;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
@@ -14,6 +29,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.libs.jline.internal.InputStreamReader;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,9 +37,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.InvalidPluginException;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.fusesource.jansi.Ansi;
 
@@ -37,19 +58,73 @@ public class Main extends JavaPlugin implements Listener{
     public static Permission permission = null;
     public static Economy economy = null;
     public static Chat chat = null;
-    //private Database sql; //dynamic pricing coming soon...
+    private Database sql; //dynamic pricing coming soon...
 	public void onEnable(){
+		sql = new SQLite(Logger.getLogger("Minecraft"), "[TsCardinal] ", this.getDataFolder().getAbsolutePath(), "pricedb", ".sqlite");
+		if(sql.open()){
+			try {
+				sql.query(sql.prepare("CREATE TABLE IF NOT EXISTS itemPricing (\"itemName\" TEXT(16), \"itemsBought\" INTEGER, \"itemsSold\" INTEGER, \"itemStartValue\" INTEGER, \"itemValue\" INTEGER); CREATE UNIQUE INDEX itemNames ON itemPricing(\"itemName\");"));
+			} catch (SQLException e) {
+				getLogger().severe("Something seems to have gone terribly wrong while setting up the item database. Dumping stacktrace...");
+				e.printStackTrace();
+			}
+		}
         setupPermissions();
         setupChat();
 		if (!setupEconomy() ) {
             getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
-        }
+		}
 		pm.registerEvents(this, Main.getPlugin(com.tsuser.tscardinal.Main.class));
 		getLogger().info(Ansi.ansi().fg(Ansi.Color.RED) + "TsCardinal has been enabled!" + resetAnsi);
 	}
-	
+	public void onDisable(){
+		sql.close();
+	}
+	public static boolean downloadPlugin(Plugin plugin, String id) {
+        InputStreamReader in = null;
+        try {
+            URL url = new URL("https://api.curseforge.com/servermods/files?projectIds=" + id);
+            URLConnection urlConnection = url.openConnection();
+            in = new InputStreamReader(urlConnection.getInputStream());
+            int numCharsRead;
+            char[] charArray = new char[1024];
+            StringBuilder sb = new StringBuilder();
+            while ((numCharsRead = in.read(charArray)) > 0) {
+                sb.append(charArray, 0, numCharsRead);
+            }
+            String result = sb.toString();
+            result = result.replace("\\/", "/").replaceAll(".*\"downloadUrl\":\"", "").split("\",\"")[0];
+            String[] split = result.split("/");
+            url = new URL(result);
+            final String path = plugin.getDataFolder().getParentFile().getAbsoluteFile() + "/" + split[split.length];
+            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            @SuppressWarnings("resource")
+			FileOutputStream fos = new FileOutputStream(path);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            Bukkit.getServer().getLogger().log(Level.INFO, "Finished downloading " + split[split.length] + ". Loading dependecy");
+            Bukkit.getServer().getPluginManager().loadPlugin(new File(path));
+            return true;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidPluginException ex) {
+            Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidDescriptionException ex) {
+            Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownDependencyException ex) {
+            Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PluginLoader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
     private boolean setupPermissions()
     {
         RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
@@ -104,6 +179,12 @@ public class Main extends JavaPlugin implements Listener{
 		        	//sign.setLine(2, ChatColor.RED + "Not ready yet");
 		        	sign.setLine(0, ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "TSC Shop" + ChatColor.DARK_GREEN + "]");
 		        	player.sendMessage(ChatColor.GREEN + "Shop created! Item: " + sign.getLine(2) + " Ammt: " + sign.getLine(1) + " Price: " + economy.format(Double.valueOf(sign.getLine(3))));
+		        	try {
+						sql.query(sql.prepare("INSERT INTO itemPricing (\"itemName\", \"itemsBought\", \"itemsSold\", \"itemStartValue\", \"itemValue\") VALUES ('" + sign.getLine(2).toUpperCase() + "', NULL, NULL, " + sign.getLine(3) + ", " + sign.getLine(3) + ");"));
+					} catch (SQLException e) {
+						player.sendMessage(ChatColor.RED + "Critical database error! Tell staff NOW!");
+						e.printStackTrace();
+					}
 		        	if(Material.getMaterial(sign.getLine(2).toUpperCase()) == null){
 		        		block.breakNaturally();
 		        		player.sendMessage(ChatColor.RED + "Sorry, but that item wasn't valid!");
@@ -116,7 +197,7 @@ public class Main extends JavaPlugin implements Listener{
 		        	player.sendMessage(ChatColor.RED + "The sign you attempted to create was invalid. Sorry.");
 		        }
 		    }
-		    if (sign.getLine(0).equalsIgnoreCase("[tsctest]")){
+		    if (sign.getLine(0).equalsIgnoreCase("[tsctest]") && player.hasPermission("tscardinal.admin")){
 		    	sign.setLine(0, ChatColor.GREEN + "-= Test =-");
 		    	sign.setLine(1, ChatColor.MAGIC + "XXXXXXXXXXXXXXX");
 		    	sign.setLine(2, ChatColor.RED + "Looking good");
@@ -136,14 +217,14 @@ public class Main extends JavaPlugin implements Listener{
 		switch (Event.getAction()){
 			case LEFT_CLICK_BLOCK:
 				if(player.getGameMode().equals(GameMode.CREATIVE) && player.hasPermission("tscardinal.admin")){
-					player.sendMessage(ChatColor.GREEN + "Sign removed sucessfully.");
+					//player.sendMessage(ChatColor.GREEN + "Sign removed successfully."); //Bug with non-sign blocks being broken w/ this message.
 				} else { 
-					Event.setCancelled(true);
 					if (block != null){
 						if (block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST) {
 							Sign sign = (Sign) block.getState();
 							String signhead = sign.getLine(0);
 							if(signhead.equalsIgnoreCase(ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "TSC Shop" + ChatColor.DARK_GREEN + "]")){
+								Event.setCancelled(true);
 								ammount = Integer.valueOf(sign.getLine(1));
 								item = Material.getMaterial(sign.getLine(2).toUpperCase());
 								price = Double.parseDouble(sign.getLine(3));
@@ -151,6 +232,27 @@ public class Main extends JavaPlugin implements Listener{
 									economy.bankWithdraw(player.getName(), price);
 									player.getInventory().addItem(new ItemStack(item, ammount));
 									player.sendMessage(ChatColor.GREEN + "You bought " + ammount + "x" + sign.getLine(2) + " for " + economy.format(price) + "!");
+									String signPrice;
+									try {
+										String itemName = sign.getLine(2).toUpperCase();
+										Connection dbcon = sql.getConnection();
+										PreparedStatement query = dbcon.prepareStatement("UPDATE itemPricing SET itemsSold=itemsSold + ? WHERE itemName='" + itemName + "';");
+										dbcon.setAutoCommit(false);
+										query.setInt(1, ammount);
+										query.executeUpdate();
+										dbcon.commit();
+										dbcon.setAutoCommit(true);
+										Double startPrice = sql.query("SELECT itemStartValue FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemStartValue");
+										Double itemsSold = sql.query("SELECT itemsSold FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemsSold");
+										Double itemsBought = sql.query("SELECT itemsBought FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemsBought");
+										Double newPrice = Math.abs(startPrice-1.25*Math.log(itemsSold-itemsBought));
+										signPrice = Double.toString(Math.round(newPrice));
+										sign.setLine(3, signPrice);
+										player.sendMessage(ChatColor.BLUE + "DEBUG: New price returned " + newPrice.toString() + "!" + itemsSold.toString() + " and " + itemsBought.toString());
+									} catch (SQLException e) {
+										player.sendMessage(ChatColor.RED + "Critical error with shop pricing, tell a GM about this please.");
+										e.printStackTrace();
+									}
 								} else {
 									player.sendMessage(ChatColor.RED + "You have insufficient funds!");
 								}
@@ -172,6 +274,27 @@ public class Main extends JavaPlugin implements Listener{
 								economy.bankDeposit(player.getName(), price);
 								player.getInventory().removeItem(new ItemStack(item, ammount));
 								player.sendMessage(ChatColor.GREEN + "You sold " + ammount + "x" + sign.getLine(2) + " for " + economy.format(price) + "!");
+								String signPrice;
+								try {
+									String itemName = sign.getLine(2).toUpperCase();
+									Connection dbcon = sql.getConnection();
+									PreparedStatement query = dbcon.prepareStatement("UPDATE itemPricing SET itemsSold=itemsSold + ? WHERE itemName='" + itemName + "';");
+									dbcon.setAutoCommit(false);
+									query.setInt(1, ammount);
+									query.executeUpdate();
+									dbcon.commit();
+									dbcon.setAutoCommit(true);
+									Double startPrice = sql.query("SELECT itemStartValue FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemStartValue");
+									Double itemsSold = sql.query("SELECT itemsSold FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemsSold");
+									Double itemsBought = sql.query("SELECT itemsBought FROM itemPricing WHERE itemName='" + sign.getLine(2).toUpperCase() + "' LIMIT 1;").getDouble("itemsBought");
+									Double newPrice = Math.abs(startPrice-1.25*Math.log(itemsSold-itemsBought));
+									signPrice = Double.toString(Math.round(newPrice));
+									sign.setLine(3, signPrice);
+									player.sendMessage(ChatColor.BLUE + "DEBUG: New price returned " + Double.toString(Math.round(newPrice)) + "! S#: " + itemsSold.toString() + " B#: " + itemsBought.toString());
+								} catch (SQLException e) {
+									player.sendMessage(ChatColor.RED + "Error with shop pricing, tell someone.");
+									e.printStackTrace();
+								}
 							} else {
 								player.sendMessage(ChatColor.RED + "You have insufficient resources to sell!");
 							}
